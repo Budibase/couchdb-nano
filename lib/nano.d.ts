086a137356ea03d08529e4bf8a35865844c49d33
Sam Rose <hello@samwho.dev>
2023-10-26 13:52:05 +0100

    Widen the key params in DocumentListParams.

--- lib/nano.d.ts
+++ lib/nano.d.ts
@@ -1272,12 +1272,12 @@ declare namespace nano {
     descending?: boolean;
 
     /** Stop returning records when the specified key is reached. */
-    endkey?: string;
+    endkey?: any;
 
     /** Stop returning records when the specified key is reached.
      *
      * end_key is an alias for endkey */
-    end_key?: string;
+    end_key?: any;
 
     /** Stop returning records when the specified document ID is reached. */
     end_key_doc_id?: string;
@@ -1293,10 +1293,10 @@ declare namespace nano {
     inclusive_end?: boolean;
 
     /** Return only documents that match the specified key. */
-    key?: string;
+    key?: any;
 
     /** Return only documents that match the specified keys. */
-    keys?: string | string[];
+    keys?: any | any[];
 
     /** Limit the number of the returned documents to the specified number. */
     limit?: number;
@@ -1314,12 +1314,12 @@ declare namespace nano {
     stale?: string;
 
     /** Return records starting with the specified key. */
-    startkey?: string;
+    startkey?: any;
 
     /** Return records starting with the specified key.
      *
      * start_key is an alias for startkey */
-    start_key?: string;
+    start_key?: any;
 
     /** Return records starting with the specified document ID. */
     start_key_doc_id?: string;
